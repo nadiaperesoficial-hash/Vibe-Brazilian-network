fc9f1a359ed2e27e0bd18f3305f8f957a88b1cbf
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-04 07:48:32 -0300

    Update cloudinary_keys.dart2

--- lib/core/utility/cloudinary_keys.dart
+++ lib/core/utility/cloudinary_keys.dart
@@ -1,5 +1,5 @@
 class CloudinaryKeys {
   static const String cloudName = 'dofar40nr';
   static const String apiKey = '351287213397222';
-  static const String uploadPreset = 'vibe';
+  static const String uploadPreset = 'vibeapp';
 }
