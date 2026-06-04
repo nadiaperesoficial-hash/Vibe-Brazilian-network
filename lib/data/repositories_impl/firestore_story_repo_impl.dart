645d2543fd8c6347010d431a281e634bbe72d4d6
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-04 11:09:13 -0300

    Add files via upload

---
+++ lib/data/repositories_impl/firestore_story_repo_impl.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:path_provider/path_provider.dart';
+import 'package:instagram/core/utility/constant.dart';
+import 'package:instagram/data/data_sources/remote/firebase_storage.dart';
+import 'package:instagram/data/data_sources/remote/user/firestore_user_info.dart';
+import 'package:instagram/data/data_sources/remote/story/firestore_story.dart';
+import 'package:instagram/data/models/child_classes/post/story.dart';
+import 'package:instagram/data/models/parent_classes/without_sub_classes/user_personal_info.dart';
+import 'package:instagram/domain/repositories/story_repository.dart';
+
+class FireStoreStoryRepositoryImpl implements FirestoreStoryRepository {
+  @override
+  Future<String> createStory(
+      {required Story storyInfo, required Uint8List file}) async {
+    try {
+      String ext = storyInfo.isThatImage ? "jpg" : "mp4";
+
+      // Salva bytes em arquivo temporário e usa uploadFile
+      final tempDir = await getTemporaryDirectory();
+      final tempFile = File('${tempDir.path}/story_upload.$ext');
+      await tempFile.writeAsBytes(file);
+
+      String postUrl = await FirebaseStoragePost.uploadFile(
+        postFile: tempFile,
+        folderName: ext,
+      );
+
+      // Deleta arquivo temporário
+      await tempFile.delete();
+
+      storyInfo.storyUrl = postUrl;
+      String storyUid = await FireStoreStory.createStory(storyInfo);
+      await FireStoreUser.updateUserStories(
+          userId: storyInfo.publisherId, storyId: storyUid);
+      return storyUid;
+    } catch (e) {
+      return Future.error(e.toString());
+    }
+  }
+
+  @override
+  Future<List<UserPersonalInfo>> getStoriesInfo(
+      {required List<dynamic> usersIds}) async {
+    try {
+      List<UserPersonalInfo> usersInfo =
+          await FireStoreUser.getSpecificUsersInfo(
+              usersIds: usersIds, userUid: myPersonalId, fieldName: 'stories');
+      return await FireStoreStory.getStoriesInfo(usersInfo);
+    } catch (e) {
+      return Future.error(e.toString());
+    }
+  }
+
+  @override
+  Future<UserPersonalInfo> getSpecificStoriesInfo(
+      {required UserPersonalInfo userInfo}) async {
+    try {
+      return (await FireStoreStory.getStoriesInfo([userInfo]))[0];
+    } catch (e) {
+      return Future.error(e.toString());
+    }
+  }
+
+  @override
+  Future<void> deleteThisStory({required String storyId}) async {
+    try {
+      await FireStoreUser.deleteThisStory(storyId: storyId);
+      await FireStoreStory.deleteThisStory(storyId: storyId);
+    } catch (e) {
+      return Future.error(e.toString());
+    }
+  }
+}
